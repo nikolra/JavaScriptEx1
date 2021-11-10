f1ea81065771cff7576b85cf722f58c4fec10ace
nikolra <nikolra@mta.ac.il>
2021-11-10 22:57:25 +0200

    one disk disappears on coalition. the disks move in a random direction. the location os the disks is random on the borders of the rectangle

--- JavaScript/Disks.js
+++ JavaScript/Disks.js
@@ -1,24 +1,27 @@
-//const canvas = document.getElementById("DiskCanvas");
-//const context = canvas.getContext("2d");
-
-
-
 
 //need to create the 4 disks
 
 function animate() {
     requestAnimationFrame(this.animate) //similar to setInterval. call as soon as it can
     context.clearRect(0, 0, canvas.width, canvas.height) // clears the previous screen. only the last drawn disk is shown
-    disks.forEach(disk => {
+    disks.forEach( (disk, index) => {
         disk.draw()
         disk.update()
+
+            disks.forEach(innerDisk => {//checks coalition between tow disks
+            //need to check that it is not the same disk!!!!
+            const distance = Math.hypot(innerDisk.x - disk.x, innerDisk.y - disk.y)
+            if(disk !== innerDisk) {
+                if (distance - innerDisk.radius - disk.radius < 1) { //tow disks collide
+                    setTimeout(() => {// removes flash on the screen when a disk is removed
+                        //TODO: decide which disk to remove
+                        disks.splice(index, 1)// removes a single disk at index in the array
+                    })
+                }
+            }
+        })
     })
 }
 
-function spawnEnemies() {
-    setInterval( () => {
-
-    }, 1000)
-}
-
 window.addEventListener('click', animate)//happens every time we click on the mouse
+//TODO: delete before submitting
