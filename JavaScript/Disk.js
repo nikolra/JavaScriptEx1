6d7225f1f05286d2b0c237f6650e4b3ddcb6c7f4
nikolra <nikolra@mta.ac.il>
2021-11-10 13:12:43 +0200

    4 disks are initialized and drawn in the rectangle

--- JavaScript/Disk.js
+++ JavaScript/Disk.js
@@ -1,20 +1,29 @@
-const canvas = document.getElementById("myCanvas");
+const canvas = document.getElementById("DiskCanvas");
 const context = canvas.getContext("2d");
 
 class Disk{
-    constructor(x,y,radius,color) {
+    constructor(x, y, radius, color, velocity) {
         this.x=x
         this.y=y
         this.radius = radius
         this.color = color
+        this.velocity = velocity
     }
 
     draw(){
         context.beginPath()
-        context.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
+        context.arc(this.x,this.y,this.radius,0,Math.PI * 2,false)
+        context.fillStyle = this.color
         context.fill()
     }
+
+    update(){
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+
+
 }
 
-const disk = new Disk(100,100,30,'pink')
-disk.draw()
+
