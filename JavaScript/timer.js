2ddb587709e34db6c4de82483696b1b46303589a
Eden Sofir <edensofir2710@gmail.com>
2021-11-15 11:58:59 +0200

    exit tab message

--- JavaScript/timer.js
+++ JavaScript/timer.js
@@ -55,3 +55,10 @@ function timer_tick()
         curr_time_disply.innerHTML = counter;
     }
 }
+window.addEventListener('beforeunload',ev => {
+    if(isAnimating === true)
+    {
+        ev.returnValue = 'Are you sure you want to leave?' ;
+        return ev.returnValue ;
+    }
+});
